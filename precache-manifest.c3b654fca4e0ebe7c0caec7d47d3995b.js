bf2f1f0dea304a48817eea628ae32fe7b4798244
josscg449 <38119088+josscg449@users.noreply.github.com>
2020-05-03 23:09:46 -0600

    Updates

---
+++ precache-manifest.c3b654fca4e0ebe7c0caec7d47d3995b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ca6390ed595f86e22323afadcc54f0",
+    "url": "/quotetweeter/index.html"
+  },
+  {
+    "revision": "36e9e0ec634b8c117968",
+    "url": "/quotetweeter/static/css/main.d965f1cc.chunk.css"
+  },
+  {
+    "revision": "e02690618473588a88e1",
+    "url": "/quotetweeter/static/js/2.a10893fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotetweeter/static/js/2.a10893fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e9e0ec634b8c117968",
+    "url": "/quotetweeter/static/js/main.1438ec86.chunk.js"
+  },
+  {
+    "revision": "628cc00f0f92a4274457",
+    "url": "/quotetweeter/static/js/runtime-main.d8e25dca.js"
+  }
+]);
